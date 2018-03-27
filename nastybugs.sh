6e3338e7c372dfe1929c6426308d76b309a6cdd7
jack4707 <36748197+jack4707@users.noreply.github.com>
2018-03-27 13:42:56 -0400

    Add files via upload

--- nastybugs.sh
+++ nastybugs.sh
@@ -7,7 +7,8 @@ if [ "$#" -ne 6 ]; then
 	exit 1
 fi
 
-sraid= $(readlink -f "$1")
+sraid=$(readlink -f "$1") 
+###JD:removed space after = in sraid assignment
 hostGen=$(readlink -f "$2")
 cardgene=$(readlink -f "$3")
 cardsnp=$(readlink -f "$4")
@@ -16,25 +17,36 @@ outdir=$(readlink -f "$6")
 
 ## Getting host genome from NCBI and create BLAST db 
 
-printf "Getting host genome and create BLAST databases\n"
-date
-mkdir $hostGen
-wget -P $hostGen ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh37_latest/refseq_identifiers/GRCh37_latest_genomic.fna.gz
-gunzip $hostGen/GRCh37_latest_genomic.fna.gz
-makeblastdb -in $hostGen/GRCh37_latest_genomic.fna -dbtype nucl -out $hostGen/hg19
+###JD:added if statement to check whether database has already been downloaded 
+if [ ! -e "$hostGen/GRCh37_latest_genomic.fna" ]; then
+  printf "Getting host genome and create BLAST databases\n"
+  date
+  mkdir $hostGen
+  wget -P $hostGen ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh37_latest/refseq_identifiers/GRCh37_latest_genomic.fna.gz
+  gunzip -v $hostGen/GRCh37_latest_genomic.fna.gz
+  makeblastdb -in $hostGen/GRCh37_latest_genomic.fna -dbtype nucl -out $hostGen/hg19
+fi
 
 ## Getting CARD Gene and SNP databases and create BLAST db
 ## https://card.mcmaster.ca/download
-printf "Getting CARD databases\n"
-date
-mkdir $cardgene $cardsnp
-wget -P $cardgene https://card.mcmaster.ca/download/0/broadstreet-v2.0.0.tar.gz
-tar xvf $cardgene/broadstreet-v2.0.0.tar.gz -C $cardgene/
-makeblastdb -in $cardgene/nucleotide_fasta_protein_homolog_model.fasta -dbtype nucl -out $cardgene/cardgenedb
-cp $cardgene/nucleotide_fasta_protein_variant_model.fasta $cardsnp/.
-makeblastdb -in $cardsnp/nucleotide_fasta_protein_variant_model.fasta -dbtype nucl -out $cardsnp/cardsnpdb
+###JD:added if statement to check card db already exists
+if [ ! -e "$cardgene/cardgenedb.nhr" ]; then
+  printf "Getting CARD databases\n"
+  date
+  mkdir $cardgene $cardsnp
+  ###JD:had to add --no-check-certificate in order to download - not sure if this is a good solution
+  wget --no-check-certificate -O $cardgene/broadstreet.tar.gz https://card.mcmaster.ca/latest/data
+  ###JD:changed download location to latest rather than specific version
+  tar xvf $cardgene/broadstreet.tar.gz -C $cardgene/
+  makeblastdb -in $cardgene/nucleotide_fasta_protein_homolog_model.fasta -dbtype nucl -out $cardgene/cardgenedb
+  cp $cardgene/nucleotide_fasta_protein_variant_model.fasta $cardsnp/.
+  makeblastdb -in $cardsnp/nucleotide_fasta_protein_variant_model.fasta -dbtype nucl -out $cardsnp/cardsnpdb
+fi
 
 mkdir $outdir
+
+###JD:added prompt notification for user peace of mind
+printf "\n\nprocessing reads..."
 
 for sra in $(cat "$1"); do
 
@@ -68,14 +80,16 @@ for sra in $(cat "$1"); do
     magicblast -num_threads $cores  -infmt fasta -query $outdir/${sra}_unmapped_read_one_trimmed -query_mate $outdir/${sra}_unmapped_read_two_trimmed -score 50 -penalty -3 -out $outdir/$sra.CARD_gene.sam -db $cardgene/cardgenedb
     magicblast -num_threads $cores  -infmt fasta -query $outdir/${sra}_unmapped_read_one_trimmed -query_mate $outdir/${sra}_unmapped_read_two_trimmed -score 50 -penalty -3 -out $outdir/$sra.CARD_snp.sam -db $cardsnp/cardsnpdb
 
-  elif [[ (-s $read1_count) ]]; then
+  ###JD:in my unpaired run, reads were coming out in read_zero, changed if statement to check in this file rather than read_one, and also clipper and magicblast to read the _zero file
+  elif [[ (-s $read0_count) ]]; then
     printf "Single End - %d %d %d\n" "$read1_count" "$read2_count" "$read0_count"
     date
-    fastx_clipper -i $outdir/${sra}_unmapped_read_one -o $outdir/${sra}_unmapped_read_one_trimmed
+    fastx_clipper -i $outdir/${sra}_unmapped_read_zero -o $outdir/${sra}_unmapped_read_zero_trimmed
     # Run magicblast using CARD Gene Homology nucleotide_fasta_protein_homolog_model.fasta 
-    magicblast -num_threads $cores -infmt fasta -query $outdir/${sra}_unmapped_read_one_trimmed -score 50 -penalty -3 -out $outdir/$sra.CARD_gene.sam -db $cardgene/cardgenedb
+    magicblast -num_threads $cores -infmt fasta -query $outdir/${sra}_unmapped_read_zero_trimmed -score 50 -penalty -3 -out $outdir/$sra.CARD_gene.sam -db $cardgene/cardgenedb
     # Run magicblast using CARD variant  - nucleotide_fasta_protein_variant_model.fasta
-    magicblast -num_threads $cores -infmt fasta -query $outdir/${sra}_unmapped_read_one_trimmed -score 50 -penalty -3 -out $outdir/$sra.CARD_snp.sam -db $cardgene/cardsnpdb 
+    ###JD:corrected cardsnpdb location to $cardsnp/
+    magicblast -num_threads $cores -infmt fasta -query $outdir/${sra}_unmapped_read_zero_trimmed -score 50 -penalty -3 -out $outdir/$sra.CARD_snp.sam -db $cardsnp/cardsnpdb 
   else 
     printf "ERROR: no reads to align to CARD databases\n"
   fi
